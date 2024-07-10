5a9f2fd3013e7f6e14556aa34984afd0af100bde
ндрей Шаев <andrew770534@gmail.com>
2024-07-11 00:38:39 +0300

    обновил main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Logger } from 'nestjs-pino';
 import { join } from 'path';
-import { ClientOptions, Transport } from '@nestjs/microservices';
+import { ClientOptions, MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
 
 const { PORT } = process.env;
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.create(AppModule);
+
+  // Запуск HTTP сервера
+  await app.listen(3000);
+
+  // Запуск gRPC сервера
+  const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: 'payment',
-      protoPath: join(__dirname, 'proto/payment.proto'),
+      protoPath: join(__dirname, './proto/payment.proto'),
+      url: '0.0.0.0:5000',
     },
-    bufferLogs: true
   });
-
-  app.useLogger(app.get(Logger))
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: false,
-  }));
-  await app.listen();
+  await grpcApp.listen();
 }
+
 bootstrap();
