4ea943cb9c2ec6eedf1afe7ada386f0f470e678a
ндрей Шаев <andrew770534@gmail.com>
2024-07-12 01:45:00 +0300

    исправил импорт на модули

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
+import { AppModule } from '@/modules/app/app.module';
 import { Logger } from 'nestjs-pino';
 import { join } from 'path';
 import { ClientOptions, MicroserviceOptions, Transport } from '@nestjs/microservices';
